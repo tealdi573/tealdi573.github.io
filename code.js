54456048cd7c4b281f1c66068c7ea5bc09b2e9bb
Kate <ktealdi14@gmail.com>
2014-05-12 14:05:51 -0700

    Please enter the commit message for your changes. Lines starting

---
+++ code.js
@@ -0,0 +1,16 @@
+pt type="text/javascript">
+                function changetext2() {
+                $("#foo1").html("Si, Ocarina es muy divertido.");
+                $("#foo1").css("color","green");
+                $("#fool").css("fontSize","50px");
+                        }
+                </script>
+
+                <p id="foo2">This is the button to my lyrics website</p>
+                <input type="button" onclick="changetext3()" value="HEY! Where are my lyrics?" />
+<script src="http://ajax.googltapis.com/ajax/libs/jquery.1.11.0.jquery.min.js"></script>
+                <script type="text/javascript"src="code.js">
+                        function changetext3() {
+                        $("#foo2").html("RIGHT HERE!:  http://tealdi573.github.io/lyrics.html");
+                        }
+                </script>
