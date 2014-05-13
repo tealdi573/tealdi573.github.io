2f6f3da2cce9abb9dafff618426fd686f101812e
Kate <ktealdi14@gmail.com>
2014-05-13 10:43:04 -0700

    code

--- code.js
+++ code.js
@@ -14,3 +14,43 @@ pt type="text/javascript">
                         $("#foo2").html("RIGHT HERE!:  http://tealdi573.github.io/lyrics.html");
                         }
                 </script>
+
+<a href="#" class="myButton">green</a>
+
+.myButton {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #1c7834), color-stop(1, #76b853));
+	background:-moz-linear-gradient(top, #1c7834 5%, #76b853 100%);
+	background:-webkit-linear-gradient(top, #1c7834 5%, #76b853 100%);
+	background:-o-linear-gradient(top, #1c7834 5%, #76b853 100%);
+	background:-ms-linear-gradient(top, #1c7834 5%, #76b853 100%);
+	background:linear-gradient(to bottom, #1c7834 5%, #76b853 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#1c7834', endColorstr='#76b853',GradientType=0);
+	background-color:#1c7834;
+	-moz-border-radius:42px;
+	-webkit-border-radius:42px;
+	border-radius:42px;
+	border:3px solid #1ea82c;
+	display:inline-block;
+	cursor:pointer;
+	color:#86eb91;
+	font-family:Arial;
+	font-size:19px;
+	padding:16px 31px;
+	text-decoration:none;
+	text-shadow:0px 0px 24px #a2db99;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #76b853), color-stop(1, #1c7834));
+	background:-moz-linear-gradient(top, #76b853 5%, #1c7834 100%);
+	background:-webkit-linear-gradient(top, #76b853 5%, #1c7834 100%);
+	background:-o-linear-gradient(top, #76b853 5%, #1c7834 100%);
+	background:-ms-linear-gradient(top, #76b853 5%, #1c7834 100%);
+	background:linear-gradient(to bottom, #76b853 5%, #1c7834 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#76b853', endColorstr='#1c7834',GradientType=0);
+	background-color:#76b853;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
